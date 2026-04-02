f19ad6561b43dfbb2962c57b399102baaeaa399a
arrahmouni <ammarrahmouni8@gmail.com>
2026-04-02 18:15:15 +0300

    Update module statuses: Add 'Platform' status to modules_statuses.json for enhanced functionality.

---
+++ Modules/Platform/app/Providers/RouteServiceProvider.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Modules\Platform\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Mcamara\LaravelLocalization\Traits\LoadsTranslatedCachedRoutes;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    use LoadsTranslatedCachedRoutes;
+
+    protected string $name = 'Platform';
+
+    protected string $moduleNamespace = 'Modules\Platform\Http\Controllers';
+
+    /**
+     * Called before routes are registered.
+     *
+     * Register any model bindings or pattern based filters.
+     */
+    public function boot(): void
+    {
+        parent::boot();
+        Route::pattern('model', '[0-9]+');
+
+        Route::pattern('id', '[0-9]+');
+    }
+
+    /**
+     * Define the routes for the application.
+     */
+    public function map(): void
+    {
+        $this->mapApiRoutes();
+        $this->mapWebRoutes();
+    }
+
+    /**
+     * Define the "web" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     */
+    protected function mapWebRoutes(): void
+    {
+        Route::middleware(['web', 'locale'])
+        ->namespace($this->moduleNamespace)
+        ->prefix(LaravelLocalization::setLocale() . '/admin')
+        ->name('platform.')
+        ->group(module_path('Platform', '/routes/web.php'));
+    }
+
+    /**
+     * Define the "api" routes for the application.
+     *
+     * These routes are typically stateless.
+     */
+    protected function mapApiRoutes(): void
+    {
+        Route::prefix('api/' . config('app.api_version'))
+        ->middleware('api')
+        ->namespace($this->moduleNamespace)
+        ->group(module_path('Platform', '/routes/api.php'));
+    }
+}
