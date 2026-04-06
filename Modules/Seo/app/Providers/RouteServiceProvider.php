a4aaeea735170d67de60407b2f9b5228b01d2733
arrahmouni <ammarrahmouni8@gmail.com>
2026-04-06 23:33:28 +0300

    Add Seo module status: Include 'Seo' in modules_statuses.json to enhance system configuration options.

---
+++ Modules/Seo/app/Providers/RouteServiceProvider.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Modules\Seo\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use Mcamara\LaravelLocalization\Traits\LoadsTranslatedCachedRoutes;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    use LoadsTranslatedCachedRoutes;
+
+    protected string $name = 'Seo';
+
+    protected string $moduleNamespace = 'Modules\Seo\Http\Controllers';
+
+    /**
+     * Called before routes are registered.
+     *
+     * Register any model bindings or pattern based filters.
+     */
+    public function boot(): void
+    {
+        parent::boot();
+        Route::pattern('model', '[0-9]+');
+        Route::pattern('id', '[0-9]+');
+    }
+
+    /**
+     * Define the routes for the application.
+     */
+    public function map(): void
+    {
+        $this->mapApiRoutes();
+        $this->mapWebRoutes();
+    }
+
+    /**
+     * Define the "web" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     */
+    protected function mapWebRoutes(): void
+    {
+        Route::middleware(['web', 'locale'])
+        ->namespace($this->moduleNamespace)
+        ->prefix(LaravelLocalization::setLocale() . '/admin')
+        ->name('seo.')
+        ->group(module_path($this->name, '/routes/web.php'));
+    }
+
+    /**
+     * Define the "api" routes for the application.
+     *
+     * These routes are typically stateless.
+     */
+    protected function mapApiRoutes(): void
+    {
+        Route::prefix('api/' . config('app.api_version'))
+        ->middleware('api')
+        ->namespace($this->moduleNamespace)
+        ->group(module_path($this->name, '/routes/api.php'));
+    }
+}
